5aa90770f2002333122558df3593917be4f302f8
Cherry Xu <cherryxuqianyun@gmail.com>
2018-09-26 14:15:31 -0700

    Header Component

--- app/containers/Header/index.js
+++ app/containers/Header/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import NavBar from '../NavBar/index';
 
-export default class Header extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+const styles = {
+  root: {
+    height: '60px',
+  },
+  img: {
+    width: '60px',
+    height: '60px',
+    float: 'left',
+    marginLeft: '20px',
+  },
+  h1: {
+    float: 'left',
+    margin: '6px 0 0 20px',
+    fontFamily: 'Poiret One, cursive',
+    fontWeight: 100,
+  },
+  clearFloat: {
+    clear: 'both',
+  },
+};
+
+export default class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div>
-        <img alt="logo" src="../../images/icon-120x120.png" />
+      <div style={styles.root}>
+        <h1 style={styles.h1}><Link to="/">TradesCo</Link></h1>
         <NavBar />
+        <div style={styles.clearFloat} />
       </div>
     );
   }
